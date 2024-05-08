e4fda51b142c5430b46e07e28783d34b3d42abee
BrunoReyes <brunorg@pop-os.localdomain>
2024-05-08 16:31:10 -0600

    Modelo integrado

--- reporta_baches_api/views/reportes/views.py
+++ reporta_baches_api/views/reportes/views.py
@@ -23,6 +23,35 @@ from reporta_baches_api.domain.reportes.services import ReportesService
 from rest_framework import status
 from rest_framework.response import Response
 
+#Librerias para modelo
+#Loading the saved_model
+import tensorflow as tf
+import time
+import numpy as np
+import warnings
+warnings.filterwarnings('ignore')
+from PIL import Image
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as viz_utils
+import random
+import matplotlib.pyplot as plt
+import os
+
+# ================== Modelo de IA ==================
+# Importando el modelo
+PATH_TO_SAVED_MODEL="./../../../mobilenet/saved_model"
+print('Loading model... \n', end='')
+# Load saved model and build the detection function
+detect_fn=tf.saved_model.load(PATH_TO_SAVED_MODEL)
+print('Done!')
+
+#Loading the label_map
+category_index=label_map_util.create_category_index_from_labelmap("./../../../label_map.pbtxt",use_display_name=True)
+
+def load_image_into_numpy_array(path):
+    return np.array(Image.open(path))
+
+# ==================================================
 
 class ReportesTrabajador(CreateLisViewSet): 
 
@@ -64,6 +93,42 @@ class ReportesTrabajador(CreateLisViewSet):
             serializer = ReporteTrabajadorSerializer(reporte)
             for image in images:
                 ImagenesTrabajador.objects.create(image=image, reporte=reporte)
+                print('Ejecutando inferencia... ')
+                
+                image_np = load_image_into_numpy_array(image)
+
+                # The input needs to be a tensor, convert it using `tf.convert_to_tensor`.
+                input_tensor = tf.convert_to_tensor(image_np)
+                # The model expects a batch of images, so add an axis with `tf.newaxis`.
+                input_tensor = input_tensor[tf.newaxis, ...]
+
+                detections = detect_fn(input_tensor)
+
+                # All outputs are batches tensors.
+                # Convert to numpy arrays, and take index [0] to remove the batch dimension.
+                # We're only interested in the first num_detections.
+                num_detections = int(detections.pop('num_detections'))
+                detections = {key: value[0, :num_detections].numpy()
+                            for key, value in detections.items()}
+                detections['num_detections'] = num_detections
+
+                # detection_classes should be ints.
+                detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
+
+                image_np_with_detections = image_np.copy()
+
+                viz_utils.visualize_boxes_and_labels_on_image_array(
+                    image_np_with_detections,
+                    detections['detection_boxes'],
+                    detections['detection_classes'],
+                    detections['detection_scores'],
+                    category_index,
+                    use_normalized_coordinates=True,
+                    max_boxes_to_draw=200,
+                    min_score_thresh=.4, # Adjust this value to set the minimum probability boxes to be classified as True
+                    agnostic_mode=False)
+                print(image_np_with_detections) # Esta es la imagen que quieres Arcadiooo 
+                
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else: 
